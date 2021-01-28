7f06a7ec0d0ebc0c813d75d1c707fe8cee6bd21b
Louis Abramson <pyreta@gmail.com>
2021-01-28 17:15:16 -0500

    delete stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
 
 class LambdaDemo extends Component {
   constructor(props) {
@@ -34,15 +32,7 @@ class LambdaDemo extends Component {
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
+      <LambdaDemo />
     )
   }
 }
